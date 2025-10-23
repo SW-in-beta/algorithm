d35ea73aae9575beb854ba40b28d7c2cd23eb7ac
이상원 <eeokee52@gmail.com>
2025-10-23 09:32:59 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -11,37 +11,24 @@ for _ in range(M):
     edges[u].add(v)
     edges[v].add(u)
 
-sorted_edges = [sorted(edge, reverse=True) for edge in edges]
-queue = deque([V])
-visited = [False] * (N + 1)
-dfs = []
-while queue:
-    e = queue.pop()
-    if visited[e]:
-        continue
-    visited[e] = True
-    dfs.append(str(e))
-
-    for node in sorted_edges[e]:
-        if visited[node]:
+def search(v, dfs=True):
+    sorted_edges = [sorted(edge, reverse=dfs) for edge in edges]
+    queue = deque([v])
+    visited = [False] * (N + 1)
+    res = []
+    while queue:
+        e = queue.pop() if dfs else queue.popleft()
+        if visited[e]:
             continue
-        queue.append(node)
+        visited[e] = True
+        res.append(str(e))
 
-sorted_edges = [sorted(edge) for edge in edges]        
-queue = deque([V])
-visited = [False] * (N + 1)    
-bfs = []
-while queue:
-    e = queue.popleft()
-    if visited[e]:
-        continue
-    visited[e] = True
-    bfs.append(str(e))
-
-    for node in sorted_edges[e]:
-        if visited[node]:
-            continue
-        queue.append(node)
+        for node in sorted_edges[e]:
+            if visited[node]:
+                continue
+            queue.append(node)
+            
+    return res
         
-print(' '.join(dfs))
-print(' '.join(bfs))
+print(' '.join(search(V)))
+print(' '.join(search(V, False)))
