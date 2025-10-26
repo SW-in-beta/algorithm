3505c8424258aad0167b3d8c317108b434314de8
이상원 <eeokee52@gmail.com>
2025-10-26 17:31:09 +0900

    [Gold V] Title: 집합의 표현, Time: 276 ms, Memory: 72404 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,28 +1,21 @@
 import sys
-sys.setrecursionlimit(1000000)
 input = lambda : sys.stdin.readline().rstrip()
+n, m = map(int, input().split())
+p = [i for i in range(n+1)]
 
-def find_top(n, setlist):
-  if setlist[n] != n:
-    setlist[n] = find_top(setlist[n], setlist)
-  return setlist[n]
+def find(n):
+    if p[n] != n:
+        p[n] = find(p[n])
+    return p[n]
 
-def union_set(n1, n2, setlist):
-  t1 = find_top(n1, setlist)
-  t2 = find_top(n2, setlist)
-  setlist[t1] = t2
-
-def in_same_set(n1, n2, setlist):
-  t1 = find_top(n1, setlist)
-  t2 = find_top(n2, setlist)
-  return t1 == t2
-
-if __name__ == '__main__':
-  n, m = map(int, input().split())
-  setlist = [i for i in range(n+1)]
-  for _ in range(m):
-    com, n1, n2 = map(int, input().split())
-    if com == 0:
-      union_set(n1, n2, setlist)
-    elif com == 1:
-      print('YES' if in_same_set(n1, n2, setlist) else 'NO')
+def union(u, v):
+    pu = find(u)
+    pv = find(v)
+    p[pv] = pu
+    
+for _ in range(m):
+    o, u, v = map(int, input().split())
+    if o:
+        print('YES' if find(u) == find(v) else 'NO')
+    else: 
+        union(u, v)
