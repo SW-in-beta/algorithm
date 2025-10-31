749cb3e8f6980b9490ab6b936d9a2228726176e2
이상원 <eeokee52@gmail.com>
2025-11-01 08:55:55 +0900

    [Gold IV] Title: Puyo Puyo, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,44 +1,51 @@
 import sys
-from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
 
-def find_idxs(r, c):
-    idxs = [(r+dr, c+dc) for dr, dc in move]
-    filtered_idxs = [(nr, nc) for nr, nc in idxs if 0 <= nr < 6 and 0 <= nc < len(board[nr])]
-    return filtered_idxs
+board = [[] for _ in range(6)]
+for j in range(-1, -13, -1):
+    for i, e in enumerate(input()):
+        if e != '.':
+            board[i] = [e] + board[i]
+        
 
-def find_bomb(board):
-    visited = [[False] * 12 for _ in range(6)]
+dx = (0, 1, 0, -1)
+dy = (-1, 0, 1, 0)
+
+def next_index(i, j):
+    return ((i + nx, j + ny) for nx, ny in zip(dx, dy) if 0 <= i + nx < 6 and 0 <= j + ny < len(board[i + nx]))
+
+def bomb(board):
     bombs = []
-    for r, row in enumerate(board):
-        for c, col in enumerate(row):
-            bomb = []
-            q = deque([(r, c)])
+    visited = [[False] * len(row) for row in board]
+    for i in range(6):
+        for j in range(len(board[i])):
+            if visited[i][j]: continue
+            color = board[i][j]
+            b = []
+            q = [(i, j)]
             while q:
-                nr, nc = q.popleft()
-                if visited[nr][nc]:
-                    continue
-                visited[nr][nc] = True
-                bomb.append((nr, nc))
-                idxs = find_idxs(nr, nc)
-                filtered_idxs = [(cr, cc) for cr, cc in idxs if board[cr][cc] == col and not visited[cr][cc]]
-                q.extend(filtered_idxs)
-            if len(bomb) >= 4:
-                bombs.append(bomb)
+                r, c = q.pop()
+                if visited[r][c]: continue
+                visited[r][c] = True
+                b.append((r, c))
+                for nr, nc in next_index(r, c):
+                    if not visited[nr][nc] and board[nr][nc] == color:
+                        q.append((nr, nc))
+            if len(b) >= 4: bombs.append(b)
     return bombs
 
-board = [list(input()) for _ in range(12)]
-board = [list(filter(lambda x: x != '.', reversed(element))) for element in zip(*board)]
-move = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+def remove(bombs, board):
+    for b in bombs:
+        for r, c in b:
+            board[r][c] = '.'
+    return [list(filter(lambda x: x != '.', row)) for row in board]
+
 cnt = 0
 while True:
-    bombs = find_bomb(board)
+    bombs = bomb(board)
     if not bombs:
         break
     cnt += 1
-    for bomb in bombs:
-        for r, c in bomb:
-            board[r][c] = '.'
-    board = [list(filter(lambda x: x != '.', row)) for row in board]
+    board = remove(bombs, board)
     
 print(cnt)
