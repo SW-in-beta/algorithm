b0804dae7fefcded9f72ce5ef612bad1df56c5af
이상원 <eeokee52@gmail.com>
2025-11-03 21:22:49 +0900

    [Gold IV] Title: 최단경로, Time: 788 ms, Memory: 96348 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,26 +1,25 @@
 import sys
-import math
 from heapq import heappush, heappop
 input = lambda : sys.stdin.readline().rstrip()
 
 V, E = map(int, input().split())
 K = int(input())
 
-es = [[] for _ in range(V+1)]
+dist = ['INF'] * (V+1)
+h = [(0, K)]
+graph = [[] for _ in range(V+1)]
 for _ in range(E):
-  u, v, w = map(int, input().split())
-  es[u].append((v, w))
+    u, v, w = map(int, input().split())
+    graph[u].append((w, v))
+    
+while h:
+    w, u = heappop(h)
+    if dist[u] != 'INF':
+        continue
+    dist[u] = w
 
-vs = [math.inf] * (V+1)
-heap = [(0, K)]
-
-while len(heap) > 0:
-  d, u = heappop(heap)
-  if vs[u] < d: continue
-  vs[u] = d
-  for v, w in es[u]:
-    if d + w >= vs[v]: continue
-    vs[v] = d + w
-    heappush(heap, (d+w, v))
-
-print('\n'.join(map(lambda x: str(x).upper(), vs[1:])))
+    for nw, v in graph[u]:
+        if dist[v] == 'INF':
+            heappush(h, (w + nw, v))
+            
+print('\n'.join(map(str, dist[1:])))
