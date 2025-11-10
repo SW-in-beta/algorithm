c8a2b2f99924ec70d0102379f2a09f2c45825a96
이상원 <eeokee52@gmail.com>
2025-11-10 21:06:56 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,23 @@
+"""
+1. 장르 별로 가장 많이 재생된 노래 두 개 씩
+2. 장르는 속한 노래가 가장 많이 재생된거 우선
+3. 장르 -> 노래. 노래 재생 횟수 같으면 고유 번호 낮은 노래 순으로.
+4. 고유번호는 인덱스
+
+그렇게 어렵진 않다. 어떻게 해야 효율적일지가 문제
+
+"""
+from collections import defaultdict as dd
+
+def solution(genres, plays):
+    g_cnt = dd(int)
+    g_list = dd(list)
+    for i, (g, p) in enumerate(zip(genres, plays)):
+        g_cnt[g] += p
+        g_list[g].append((p, -i))
+    
+    play_list = []
+    for _, genre in sorted(((c, g) for g, c in g_cnt.items()), reverse=True):
+        play_list.extend([-i for p, i in sorted(g_list[genre], reverse=True)][:2])
+        
+    return play_list
