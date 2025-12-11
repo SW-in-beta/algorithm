366462e00223f6e356edf09747648da7df17f547
이상원 <eeokee52@gmail.com>
2025-12-11 09:28:13 +0900

    [Gold IV] Title: LCS 2, Time: 928 ms, Memory: 43848 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,25 +1,12 @@
-import sys
-sys.setrecursionlimit(10**6)
+s1 = '0' + input()
+s2 = '0' + input()
+dp = [[''] * len(s2) for _ in range(len(s1))]
 
-s1 = input()
-s2 = input()
+for i in range(1, len(s1)):
+    for j in range(1, len(s2)):
+        dp[i][j] = dp[i-1][j] if len(dp[i-1][j]) >= len(dp[i][j-1]) else dp[i][j-1]
+        if s1[i] == s2[j]:
+            dp[i][j] = dp[i-1][j-1] + s1[i] if len(dp[i-1][j-1]) >= len(dp[i][j]) else dp[i][j]
 
-lcs = [[None] * len(s2) for _ in s1]
-
-def find_lcs(i, j):
-    if i < 0 or j < 0:
-        return ''
-    if lcs[i][j] != None:
-        return lcs[i][j]
-    flag = s1[i] if s1[i] == s2[j] else ''
-    if flag:
-        base = find_lcs(i-1, j-1)
-    else:
-        left = find_lcs(i-1, j)
-        right = find_lcs(i, j-1)
-        base = left if len(left) >= len(right) else right
-    lcs[i][j] = base + flag
-    return lcs[i][j]
-l = find_lcs(len(s1) - 1, len(s2) - 1)
-print(len(l))
-print(l)
+print(len(dp[-1][-1]))
+print(dp[-1][-1])
